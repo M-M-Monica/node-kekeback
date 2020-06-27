c0ddf37fe155317104dcb2176bbcc04c1a6b7584
M-M-Monica <moyingqideyouxiang@163.com>
2020-06-27 11:22:01 +0800

    sql

---
+++ core/config.js
@@ -0,0 +1,23 @@
+//配置环境
+module.exports = {
+	//prod生产环境
+	//dev开发环境
+	environment: 'dev',
+	database: {
+		dbName: 'kekeback',
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: ''
+	},
+  security: {
+    secretKey: "Molly_#137",
+    expiresIn: 60*60*24*30
+  },
+  wx: {
+    appId: '',
+    appSecret: '',
+    loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
+  },
+  host: 'http://localhost:3000/'
+}
